47ea56c7fd5da51c77d1acc1cd7bb713fb974eaf
James Ducay <jpducay@up.edu.ph>
2024-03-14 23:16:38 +0800

    Minor changes in login and signup

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -98,13 +98,37 @@ class PagesController < ApplicationController
     end
 
     def logindept
+        username = params[:username]
+        email = params[:email];
+        password = params[:password];
+        firebase_url = "https://isko-info-default-rtdb.asia-southeast1.firebasedatabase.app"
+        firebase = Firebase::Client.new(firebase_url)
+        
         uri = URI("https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=#{Rails.application.credentials.firebase_api_key}")
-        response = Net::HTTP.post_form(uri, "email": @email, "password": @password)
+        response = Net::HTTP.post_form(uri, "email": email, "password": password)
         data = JSON.parse(response.body)
-        if response.is_a?(Net::HTTPSuccess)
+        if response.is_a?(Net::HTTPSuccess) 
+            session[:email] = email
             session[:user_id] = data["localId"]
             session[:data] = data
-            redirect_to home_path, notice: "You have successfully logged in!"
+            flash[:notice] = "You have successfully logged in!"
+            redirect_to "/profilepage"
+        elsif email
+            firebase_response = firebase.get("user/users/" + email)
+            firebase_response_data = firebase_response.body
+            email = firebase_response_data["email"]
+            response = Net::HTTP.post_form(uri, "email": email, "password": password)
+            data = JSON.parse(response.body)
+            if response.is_a?(Net::HTTPSuccess)
+                session[:email] = email
+                session[:user_id] = data["localId"]
+                session[:data] = data
+                flash[:notice] = "You have successfully logged in!"
+                redirect_to "/profilepage"
+            else
+                flash[:notice] = "Invalid email or password"
+                redirect_to "/logindept"
+            end
         end
     end
 
@@ -122,7 +146,8 @@ class PagesController < ApplicationController
             session[:email] = email
             session[:user_id] = data["localId"]
             session[:data] = data
-            redirect_to "/profilepage", notice: "You have successfully logged in!"
+            flash[:notice] = "You have successfully logged in!"
+            redirect_to "/profilepage"
         elsif email
             firebase_response = firebase.get("user/users/" + email)
             firebase_response_data = firebase_response.body
@@ -133,12 +158,14 @@ class PagesController < ApplicationController
                 session[:email] = email
                 session[:user_id] = data["localId"]
                 session[:data] = data
-                redirect_to "/profilepage", notice: "You have successfully logged in!"
+                flash[:notice] = "You have successfully logged in!"
+                redirect_to "/profilepage"
+            else
+                flash[:notice] = "Invalid email or password"
+                redirect_to "/loginstudent"
             end
         end
           # Now you can use `user_data` to access the fetched user data
-        
-
     end
 
     def logout
