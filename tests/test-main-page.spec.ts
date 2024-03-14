dc042664187d1c0309747c9cdadacf08fcf34c36
boladss <anton.boladss@gmail.com>
2024-03-15 02:23:04 +0800

    test: update existing test cases

--- tests/test-main-page.spec.ts
+++ tests/test-main-page.spec.ts
@@ -8,21 +8,20 @@ test('main: load page', async ({ page }) => {
 
 test('main: click dept-login', async ({ page }) => {
   await page.goto('http://127.0.0.1:3000/');
-  await page.getByRole('link', { name: 'Login as Department' }).click();
-  await expect(page.getByText('LOGIN AS DEPARTMENT')).toBeVisible();
-  await expect(page.getByRole('paragraph')).toContainText('LOGIN AS DEPARTMENT');
+  await page.getByRole('link', { name: 'Log in as Department' }).click();
+  await expect(page.getByText('Log in as Department')).toBeVisible();
+  await expect(page.getByRole('heading')).toContainText('Log in as Department');
 });
 
 test('main: click student-login', async ({ page }) => {
   await page.goto('http://127.0.0.1:3000/');
-  await page.getByRole('link', { name: 'Login as Student' }).click();
-  await expect(page.getByText('LOGIN AS STUDENT')).toBeVisible();
-  await expect(page.getByRole('paragraph')).toContainText('LOGIN AS STUDENT');
+  await page.getByRole('link', { name: 'Log in as Student' }).click();
+  await expect(page.getByText('Log in as Student')).toBeVisible();
+  await expect(page.getByRole('heading')).toContainText('Log in as Student');
 });
 
 test('main: click student-signup', async ({ page }) => {
   await page.goto('http://127.0.0.1:3000/');
   await page.getByRole('link', { name: 'No account yet? Sign up here' }).click();
-  await expect(page.getByRole('paragraph')).toBeVisible();
-  await expect(page.getByRole('paragraph')).toContainText('SIGNUP');
+  await expect(page.getByRole('heading')).toContainText('Sign up as Student');
 });
