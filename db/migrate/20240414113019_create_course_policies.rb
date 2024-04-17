33d909852994376979df94af16492bed03bec732
boladss <anton.boladss@gmail.com>
2024-04-17 22:10:27 +0800

    build!: drop tables before creating new ones
    not dropping was initially leading to migration issues (i.e., SQLite3 was seeing duplicate tables each time)

--- db/migrate/20240414113019_create_course_policies.rb
+++ db/migrate/20240414113019_create_course_policies.rb
@@ -1,5 +1,6 @@
 class CreateCoursePolicies < ActiveRecord::Migration[7.1]
   def change
+    drop_table :course_policies
     create_table :course_policies do |t|
       t.string :course_title
       t.text :course_description
