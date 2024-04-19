f4850eb51f99ec9ac34950d8ffe1e958bf2615ba
boladss <anton.boladss@gmail.com>
2024-04-19 10:26:26 +0800

    fix: force user to authenticate before accessing dept profiles

--- app/controllers/course_policies_controller.rb
+++ app/controllers/course_policies_controller.rb
@@ -1,4 +1,7 @@
 class CoursePoliciesController < ApplicationController
+
+    before_action :authenticate_user
+
     def index
         @course_policies = CoursePolicy.all
         puts @course_policies.inspect 
@@ -25,6 +28,9 @@ class CoursePoliciesController < ApplicationController
     private
     def course_policy_params
         params.require(:course_policy).permit(:course_title, :course_description, :prerogative_policy, :waitlisting_schedule, :cancellation_procedure, :other_information)
-    
+    end
+
+    def authenticate_user
+        redirect_to "/loginstudent", notice: "You must be logged in to access this page." unless current_user
     end
 end
